aca9baa35c23519ece95a3c8a8e9890e26a9eeea
t.bretones <titouan.bretones@gmail.com>
2023-05-15 17:26:16 +0200

    catégories id console

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,14 +1,47 @@
-const buttons = document.querySelectorAll('.category__button')
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function() {
-    for (let j = 0; j < buttons.length; j++) {
-        buttons[j].classList.remove("active")
+async function getCategories() {
+    // Les données du Swagger
+    const datas = [
+      {
+        "id": 1,
+        "name": "Objets"
+      },
+      {
+        "id": 2,
+        "name": "Appartements"
+      },
+      {
+        "id": 3,
+        "name": "Hotels & restaurants"
+      }
+    ];
+  
+    // On boucle sur les datas et on peuple la page HTML
+    for (const data of datas) {
+      const button = document.createElement('button');
+      button.className = 'category__button';
+      button.dataset.category = data.id;
+      button.textContent = data.name;
+  
+      button.addEventListener('click', function() {
+        const categoryId = this.dataset.category;
+        console.log(categoryId);
+      });
+  
+      document.querySelector('.category__placement').appendChild(button);
     }
-    this.classList.add("active")
-    console.log('click')
-  });
-}
+  }
+  
+  // On click sur la catégorie avec l'id 1 :
+        // On masque TOUS les travaux
+        // On affiche uniquement les travaux avec la catégorie 1
+        // document.querySelector('figure').style.display = "none";
+        // const figure = document.querySelectorAll('figure')
+        // figure.forEach(el => {
+        //     el.style.display = "none";
+        // })
+        // Javascript select data attribute
+        // document.querySelector('figure:[data-category=1]')
 
-
-
+  // Appel de la fonction pour récupérer et afficher les catégories
+  getCategories();
+  
