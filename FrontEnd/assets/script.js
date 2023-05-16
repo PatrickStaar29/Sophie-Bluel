5b2b0a66ef31cc5d645c6b676a4976dfa8fe42ad
t.bretones <titouan.bretones@gmail.com>
2023-05-16 20:45:13 +0200

    page connexion + avance bouton

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,35 +1,35 @@
 async function getCategories() {
-    // Les données du Swagger
-    const datas = [
-      {
-        "id": 1,
-        "name": "Objets"
-      },
-      {
-        "id": 2,
-        "name": "Appartements"
-      },
-      {
-        "id": 3,
-        "name": "Hotels & restaurants"
-      }
-    ];
+  const req = await fetch('http://localhost:5678/api/categories')
+  const datas = await req.json()
   
     // On boucle sur les datas et on peuple la page HTML
     for (const data of datas) {
-      const button = document.createElement('button');
-      button.className = 'category__button';
-      button.dataset.category = data.id;
-      button.textContent = data.name;
+      const button = document.createElement('button')
+      button.className = 'category__button'
+      button.dataset.category = data.id
+      button.textContent = data.name
   
       button.addEventListener('click', function() {
-        const categoryId = this.dataset.category;
-        console.log(categoryId);
-      });
-  
+        const categoryId = this.dataset.category
+        console.log(categoryId)
+
+        const buttons = document.querySelectorAll('.category__button')
+          for (let j = 0; j < buttons.length; j++) {
+                buttons[j].classList.remove("active")
+            }
+            this.classList.add("active")
+            console.log('click')
+        
+        })
+
+        const figure = document.querySelectorAll('figure')
+
+         
       document.querySelector('.category__placement').appendChild(button);
+
+      
     }
-  }
+}
   
   // On click sur la catégorie avec l'id 1 :
         // On masque TOUS les travaux
@@ -43,5 +43,15 @@ async function getCategories() {
         // document.querySelector('figure:[data-category=1]')
 
   // Appel de la fonction pour récupérer et afficher les catégories
-  getCategories();
+getCategories()
+
+async function getCategorieTableau() {
+  const tabl = await fetch('http://localhost:5678/api/works')
+  const trie = await tabl.json()
+
+
+}
+
+  
+
   
