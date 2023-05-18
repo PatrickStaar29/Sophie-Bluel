3e7b1fa256951ae2333b63f53695d75ca5db752d
t.bretones <titouan.bretones@gmail.com>
2023-05-18 15:15:08 +0200

    filtre fonctionnel

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,34 +2,6 @@ async function getCategories() {
   const req = await fetch('http://localhost:5678/api/categories')
   const datas = await req.json()
 
-    // On boucle sur les datas et on peuple la page HTML
-    // for (const data of datas) {
-    //   const button = document.createElement('button')
-    //   button.className = 'category__button'
-    //   button.dataset.category = data.id
-    //   button.textContent = data.name
-  
-    //   button.addEventListener('click', function() {
-    //     const categoryId = this.dataset.category
-    //     console.log(categoryId)
-
-    //     const buttons = document.querySelectorAll('.category__button')
-    //       for (let j = 0; j < buttons.length; j++) {
-    //             buttons[j].classList.remove("active")
-    //         }
-    //         this.classList.add("active")
-    //         console.log('click')
-        
-    //     })
-
-    //     const figure = document.querySelectorAll('figure')
-
-         
-    //   document.querySelector('.category__placement').appendChild(button);
-
-      
-    // }
-
     // On boucle sur les datas pour faire le peuplage de la page
     for(const data of datas){
       // On fait directement la structure comme en html
@@ -89,28 +61,21 @@ function showWorkFiltered(idCategory){
   }
 
 }
-  
-  // On click sur la catégorie avec l'id 1 :
-        // On masque TOUS les travaux
-        // On affiche uniquement les travaux avec la catégorie 1
-        // document.querySelector('figure').style.display = "none";
-        // const figure = document.querySelectorAll('figure')
-        // figure.forEach(el => {
-        //     el.style.display = "none";
-        // })
-        // Javascript select data attribute
-        // document.querySelector('figure:[data-category=1]')
 
-  // Appel de la fonction pour récupérer et afficher les catégories
 getCategories()
 
 async function getAllWorks() {
   const test = await fetch('http://localhost:5678/api/works')
   const tablou = await test.json()
-  // Fait l'appel à l'API : http://localhost:5678/api/works
-  console.table(tablou)
-  // Parcourir le tableau avec for
-
+  console.log(tablou)
+  for (const wawa of tablou){
+    const figure = `<figure data-category="${wawa.categoryId}"><img src="${wawa.imageUrl}" alt="${test}">
+    <figcaption>${wawa.title}</figcaption><figure>`
+     
+      
+    document.querySelector('.gallery').insertAdjacentHTML('beforeend', figure)
+  }
+  // On ajoute le bouton sur le DOM
   // Créer les éléments
 
   /*
