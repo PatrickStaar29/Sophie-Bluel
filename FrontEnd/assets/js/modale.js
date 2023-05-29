5bc8f20572a2995edc53d49b7c4592136a6d9457
t.bretones <titouan.bretones@gmail.com>
2023-05-29 17:48:50 +0200

    modale début

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -1,26 +1,29 @@
 const token = localStorage.getItem('token')
 
-const modale = document.querySelector('.modale')
+const barreModif = document.querySelector('.barre_modif')
 
 const loginButton = document.querySelector('#login')
 const logoutButton = document.querySelector('#logout')
 
-const modaleModif = document.getElementById('mod_dif')
+const modifDesign = document.querySelector('.modif_design')
+const modifProjet = document.querySelector('.modif_projet')
 
 const filtre = document.querySelector('.category__placement')
 
 if (token){
-    modale.style.display = 'flex'
+    barreModif.style.display = 'flex'
     loginButton.style.display = 'none'
     logoutButton.style.display = 'block'
     filtre.style.display = 'none'
-    modaleModif.style.display = 'block'
+    modifDesign.style.display = 'block'
+    modifProjet.style.display = 'block'
 }else{
-    modale.style.display = 'none'
+    barreModif.style.display = 'none'
     loginButton.style.display = 'block'
     logoutButton.style.display = 'none'
     filtre.style.display = 'flex'
-    modaleModif.style.display = 'none'
+    modifDesign.style.display = 'none'
+    modifProjet.style.display = 'none'
 }
 
 logoutButton.addEventListener('click', (e)=>{
