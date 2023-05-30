a0fced4bd3f7b1d03baf3e942e0a1e872be77c0c
t.bretones <titouan.bretones@gmail.com>
2023-05-30 18:59:12 +0200

    modale avec image

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -30,3 +30,54 @@ logoutButton.addEventListener('click', (e)=>{
     localStorage.removeItem('token')
 })
     
+
+const modale = document.querySelector('.modale')
+    modale.style.display = 'none'
+
+const modaleBackground = document.querySelector('.modale_background')
+    modaleBackground.style.display = 'none'
+
+function openModal() {
+    const modalElements = document.querySelectorAll('.modale, .modale_background')
+        
+    modalElements.forEach(function(element) {
+        element.style.display = 'flex'
+    })
+}
+function closeModal() {
+    const modalElements = document.querySelectorAll('.modale, .modale_background')
+        
+    modalElements.forEach(function(element) {
+        element.style.display = 'none'
+    })
+}
+
+const modalButtons = document.querySelectorAll('.mod')
+
+modalButtons.forEach(function(a) {
+    a.addEventListener('click', openModal)
+})
+
+modaleBackground.addEventListener('click', closeModal)
+
+async function getAllWorks() {
+    const figworks = await fetch('http://localhost:5678/api/works')
+    const tabl = await figworks.json()
+    console.log(tabl)
+    for (const figworks of tabl){
+      const modalePeuple = `<figure data-category="${figworks.categoryId}"><img class='modale_img' src="${figworks.imageUrl}" alt="${figworks}">
+      <figure>`
+       
+        
+      document.querySelector('.modale_peuple').insertAdjacentHTML('beforeend', modalePeuple)
+    }
+    // On ajoute le bouton sur le DOM
+    // Créer les éléments
+  
+    /*
+        console.log()
+        console.table()
+    */
+  }
+  
+  getAllWorks()
